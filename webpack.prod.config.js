cb2106cebd932d7e90ae862fbe187cd22d3640ca
®© <mengwei@papayamobile.cn>
2016-10-31 10:35:36 +0800

    fix prod release file url

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -5,7 +5,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
-var extractCss = new ExtractTextPlugin('style/[name].[contenthash:8].css');
+var extractCss = new ExtractTextPlugin('/static/style/[name].[contenthash:8].css');
 
 var sBase = './src/mods/';
 
@@ -16,8 +16,8 @@ module.exports = {
     },
     output: {
         path: './dist',
-        filename: '[name].[chunkhash:8].js',
-        chunkFilename: "[name].[chunkhash:8].js"
+        filename: '/static/scripts/[name].[chunkhash:8].js',
+        chunkFilename: "/static/scripts/[name].[chunkhash:8].js"
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -68,7 +68,7 @@ module.exports = {
             {test: /\.css$/, loader: extractCss.extract('style','css')},
             {test: /\.scss$/, loader: extractCss.extract('css!sass')},
             {test: /\.(html|tpl)$/, loader: 'html-loader'},
-             {
+            {
                 test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
                 loader: 'url',
                 query: {
@@ -80,7 +80,8 @@ module.exports = {
                 test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
                 loader: 'url',
                 query: {
-                    limit: 10000
+                    limit: 10000,
+                    name:'static/fonts/[name].[ext]'
                 }
             }     
         ]
