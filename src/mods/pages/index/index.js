71dd56a04f76d658930c4c5a8cd6aef91284b3ef
®© <mengwei@papayamobile.cn>
2016-11-01 17:56:13 +0800

    move router to index.js

--- src/mods/pages/index/index.js
+++ src/mods/pages/index/index.js
@@ -1,10 +1,46 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import { sync } from 'vuex-router-sync';
 import store from './vuex/store';
-import router from './route/router';
 import footer from 'widget/footer/footer';
+import Notfound from './widget/error/notfound';
+import Trend from './widget/trend/trend';
+import Article from './widget/article/article';
 
-var App = new Vue({
+Vue.config.debug = true;
+Vue.use(VueRouter);
+
+// const Trend = resolve => {
+//     require.ensure(['../widget/trend/trend'],() => {
+//         resolve(require('../widget/trend/trend'));
+//     });
+// };
+
+// const Article = resolve => {
+//     require.ensure(['../widget/article/article'],() => {
+//         resolve(require('../widget/article/article'));
+//     })
+// };
+
+// const Notfound = resolve => {
+//     require.ensure(['./widget/error/notfound'],() => {
+//         resolve(require('./widget/error/notfound'));
+//     })
+// };
+
+const router = new VueRouter({
+    mode:'hash',
+    routes: [
+        { path: '/', component: Trend },
+        { path: '/p/:article_id', component: Article, name:'article'},
+        { path: '/error/notfound', component: Notfound },
+        { path: '/:type/:cate', component: Trend},
+        { path: '/', redirect: '/hot/now'},
+        { path: '*', redirect: '/error/notfound'}
+    ]
+});
+
+const App = new Vue({
     router,
     store,
     template:`
