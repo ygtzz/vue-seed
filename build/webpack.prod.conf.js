7d943cecfe11d15a1e8f3e8930c52ce09898add7
®© <mengwei@papayamobile.cn>
2016-11-01 09:58:36 +0800

    prod config add htmlwebpack generator code

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -5,24 +5,13 @@ var merge = require('webpack-merge');
 var baseWebapckConfig = require('./webpack.base.conf');
 var config = require('./config');
 
-var plugins = [
-    new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: config.sBase + 'pages/index/index.html',
-        chunks: ['index', 'vue.chunk'],
-        inject: 'body',
-        title: 'Index Page'
-    }),
-    new HtmlWebpackPlugin({
-        filename: 'home.html',
-        template: config.sBase + 'pages/home/home.html',
-        chunks: ['home', 'vue.chunk'],
-        inject: 'body',
-        title: 'Home Page'
-    }),
+var oEntry = baseWebapckConfig.entry,
+    aEntry = Object.keys(oEntry);
+
+var aPlugin = [
     new webpack.optimize.CommonsChunkPlugin({
-        name: 'vue.chunk',
-        chunks: ['index', 'home'],
+        name: 'common',
+        chunks: aEntry,
         minChunks: 2
     }),
     new ExtractTextPlugin('/static/style/[name].[contenthash:8].css'),
@@ -39,24 +28,15 @@ var plugins = [
     new webpack.optimize.OccurenceOrderPlugin()
 ];
 
-var entry = baseWebapckConfig.entry;
-//chunk
 //html webpack
-Object.keys(entry).forEach(function(item){
-       plugins.push(new HtmlWebpackPlugin({
-            filename: item + '.html',
-			template: config.sBase + 'pages/'+ item + '/' + item +'.html',
-            chunks: [item],
-            inject: 'body',
-            title: item + 'Page'
-		}));
-        new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: config.sBase + 'pages/index/index.html',
-        chunks: ['index', 'vue.chunk'],
+aEntry.forEach(function(item){
+    aPlugin.push(new HtmlWebpackPlugin({
+        filename: item + '.html',
+        template: config.sBase + 'pages/' + item + '/' + item + '.html',
+        chunks: [item, 'common'],
         inject: 'body',
-        title: 'Index Page'
-    }),
+        title: item + 'Page'
+    }));
 });
 
 module.exports = merge(baseWebapckConfig,{
@@ -65,38 +45,6 @@ module.exports = merge(baseWebapckConfig,{
         filename: '/static/scripts/[name].[chunkhash:8].js',
         chunkFilename: "/static/scripts/[name].[chunkhash:8].js"
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-			template: config.sBase + 'pages/index/index.html',
-            chunks: ['index','vue.chunk'],
-            inject: 'body',
-            title: 'Index Page'
-		}),
-        new HtmlWebpackPlugin({
-            filename: 'home.html',
-			template: config.sBase + 'pages/home/home.html',
-            chunks: ['home','vue.chunk'],
-            inject: 'body',
-            title: 'Home Page'
-		}),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vue.chunk',
-            chunks: ['index','home'],
-            minChunks: 2            
-        }),
-        new ExtractTextPlugin('/static/style/[name].[contenthash:8].css'),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress:{
-                warnings: false
-            }
-        }),
-        new webpack.optimize.OccurenceOrderPlugin()
-	],
+    plugins:aPlugin,
     devtool: 'cheap-module-source-map'
 });
