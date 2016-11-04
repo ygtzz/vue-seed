0b55250fc59c1dfe14f91a0cc597dbc424591541
®© <mengwei@papayamobile.cn>
2016-11-04 18:24:41 +0800

    prod mode common chunck split

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -12,7 +12,11 @@ var aPlugin = [
     new webpack.optimize.CommonsChunkPlugin({
         name: 'common',
         chunks: aEntry,
-        minChunks: 2
+        minChunks: 3
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        chunks: ['vendor', 'common']
     }),
     new ExtractTextPlugin('/static/style/[name].[contenthash:8].css'),
     new webpack.DefinePlugin({
@@ -29,22 +33,34 @@ var aPlugin = [
 ];
 
 //html webpack
-aEntry.forEach(function(item){
+aEntry.forEach(function(item) {
     aPlugin.push(new HtmlWebpackPlugin({
         filename: item + '.html',
         template: config.sBase + 'pages/' + item + '/' + item + '.html',
-        chunks: [item, 'common'],
+        chunks: [item, 'vendor', 'common'],
         inject: 'body',
-        title: item + 'Page'
+        title: item + 'Page',
+        minify: {
+            removeComments: true,
+            collapseWhitespace: true,
+            removeAttributeQuotes: true
+            // more options:
+            // https://github.com/kangax/html-minifier#options-quick-reference
+        },
+        // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+        chunksSortMode: 'dependency'
     }));
 });
 
-module.exports = merge(baseWebapckConfig,{
+module.exports = merge(baseWebapckConfig, {
+    entry: {
+        vendor: ['vue', 'vuex', 'vue-router', 'vuex-router-sync']
+    },
     output: {
         path: './dist',
         filename: '/static/scripts/[name].[chunkhash:8].js',
         chunkFilename: "/static/scripts/[name].[chunkhash:8].js"
     },
-    plugins:aPlugin,
+    plugins: aPlugin,
     devtool: 'cheap-module-source-map'
 });
