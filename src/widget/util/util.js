ef58fbee3c0abb3aa082ee87db762422fd32c675
ygtzz <ygtzz@126.com>
2016-11-12 19:21:34 +0800

    modify util function export mode

--- src/widget/util/util.js
+++ src/widget/util/util.js
@@ -1,3 +1,3 @@
-export const fFormatUtcDate = function(sUtcDateString){
+export function fFormatUtcDate(sUtcDateString){
     return sUtcDateString.replace('T',' ').replace('Z','').replace(/-/g,'/');
 }
